f54a084b539336554694cde32ef08e519e3c1f2f
Cherry Kim <88839700+CherryCom@users.noreply.github.com>
2025-11-14 09:54:49 -0500

    yea boi

--- answer/main.c
+++ answer/main.c
@@ -4,31 +4,34 @@
 
 void taskFirst(void *p) {
     int count = 0;
-    while (1) {
+    while (count < 5) {
         printf("hello task1: count %d\n", count++);
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
+    vTaskDelete(NULL);
 }
 
 void taskSecond(void *p) {
     int count = 0;
-    while (1) {
+    while (count < 5) {
         printf("hello task2: count %d\n", count++);
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
+    vTaskDelete(NULL);
 }
 
 void taskThird(void *p) {
     int count = 0;
-    while (1) {
+    while (count < 5) {
         printf("hello task3: count %d\n", count++);
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
+    vTaskDelete(NULL);
 }
 
 void app_main(void)
 {
-    xTaskCreate(taskFirst, "task1", 2048, NULL, 1, NULL);
+    xTaskCreate(taskFirst,  "task1", 2048, NULL, 1, NULL);
     xTaskCreate(taskSecond, "task2", 2048, NULL, 1, NULL);
-    xTaskCreate(taskThird, "task3", 2048, NULL, 1, NULL);
+    xTaskCreate(taskThird,  "task3", 2048, NULL, 1, NULL);
 }
